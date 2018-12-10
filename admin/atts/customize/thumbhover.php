e957c86367152c1ccbbcdac973cf53344b98e4f2
Christian Ong <ntrdxit@gmail.com>
2018-12-09 23:09:49 -0800

    Add semplice4 files to semplice-wp

---
+++ admin/atts/customize/thumbhover.php
@@ -0,0 +1,19 @@
+<?php
+
+// -----------------------------------------
+// semplice
+// admin/atts/customize/thumbhover.php
+// -----------------------------------------
+
+$thumbhover = array(
+	'tabs' => array(
+		'options' => array(
+			'thumb-hover-scale' => get_thumb_hover_options('scale', $fonts),
+			'thumb-hover-background' => get_thumb_hover_options('background', $fonts),
+			'thumb-hover-title-category' => get_thumb_hover_options('title-category', $fonts),
+			'thumb-hover-title' => get_thumb_hover_options('title', $fonts),	
+			'thumb-hover-category' => get_thumb_hover_options('category', $fonts),
+			'thumb-hover-shadow' => get_thumb_hover_options('shadow', $fonts),
+		),
+	),
+);
